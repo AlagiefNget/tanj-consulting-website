0be3feb684a6531df58c6468d90c4c62c5f84a06
Alagie F. Nget <anget@insistglobal.com>
2021-06-21 16:46:32 +0000

    Removed some commented code

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -99,14 +99,6 @@ const Main = () => {
                             <div className="swiper-slide"><img src="assets/img/tanjPart_3.jpg" className="img-fluid" alt="" /></div>
                             <div className="swiper-slide"><img src="assets/img/tanjPart_4.jpg" className="img-fluid" alt="" /></div>
                             <div className="swiper-slide"><img src="assets/img/tanjPart_5.jpg" className="img-fluid" alt="" /></div>
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-1.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-2.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-3.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-4.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-5.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-6.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-7.png" className="img-fluid" alt="" /></div> */}
-                            {/* <div className="swiper-slide"><img src="assets/img/clients/client-8.png" className="img-fluid" alt="" /></div> */}
                         </div>
                         <div className="swiper-pagination"></div>
                     </div>
